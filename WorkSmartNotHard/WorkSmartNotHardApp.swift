d083619249b84abd8d02b809dbbebbed7615292d
Ioannis Levakos <ioannis.levakos@gmail.com>
2025-12-24 18:10:43 +0200

    Initial Commit

---
+++ WorkSmartNotHard/WorkSmartNotHardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkSmartNotHardApp.swift
+//  WorkSmartNotHard
+//
+//  Created by Ioannis Levakos on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkSmartNotHardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
